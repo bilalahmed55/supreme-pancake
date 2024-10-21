b68e67eca651afbb93d0e01a7497bbb89de65e83
Syed Bilal Ahmed <syedbilal@Syeds-MacBook-Pro.local>
2024-10-21 13:03:03 +0500

    added basics folder

---
+++ basics/02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict"; //treat all JS code as a newer version
+
+//alert("hello"); we are using node js and not browser so it will not work
+
+/* 
+string
+number
+boolean
+null
+undefined
+symbol => unique
+
+object => 
+*/
+
+console.log(typeof "bilal");
+
+// null is an object
+// undefined output is undefined
